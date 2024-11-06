095e4f8d8f3eaeaa038b440a9a9103d25f5f3855
jy0042 <sjy6630@gmail.com>
2024-11-06 14:23:24 +0900

    11월 6일 was crud 구현

--- 4.nodejs/7.was/3.crud/crud.js
+++ 4.nodejs/7.was/3.crud/crud.js
@@ -1,6 +1,10 @@
 import http from "http";
 import { promises as fs } from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const users = {};
 
@@ -24,17 +28,41 @@ const server = http.createServer((req, res) => {
 async function handleGetRequest(req, res) {
   try {
     if (req.url === "/") {
-      const data = await fs.readFile("./index.html", "utf-8");
+      const data = await fs.readFile("./index.html");
       res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
       res.end(data);
     } else if (req.url === "/about") {
       const data = await fs.readFile("./about.html", "utf-8");
       res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
       res.end(data);
+    } else if (req.url.startsWith("/image/")) {
+      const imgName = path.basename(req.url);
+
+      if (imgName === "cat.jpeg") {
+        res.writeHead(302, { Location: "/image/faker.jpeg" });
+        res.end();
+      } else {
+        const imgPath = path.join("static", imgName);
+        const imgData = await fs.readFile(imgPath);
+        res.writeHead(200, { "Content-Type": "img/jpeg" });
+        res.end(imgData);
+      }
     } else if (req.url === "/user") {
       // console.log(users);
       res.writeHead(200, { "Content-Type": "application/json; charset=utf-8" });
       res.end(JSON.stringify(users));
+    } else if (req.url.startsWith("/static")) {
+      // static 파일들을 전달
+      const filePath = path.join(__dirname, req.url);
+      try {
+        const data = await fs.readFile(filePath);
+
+        res.writeHead(200, { "Content-Type": "application/javascript; charset=utf-8" });
+        res.end(data);
+      } catch {
+        res.writeHead(404);
+        res.end("Not Found");
+      }
     } else {
       res.writeHead(404);
       res.end("Not Found");
@@ -62,7 +90,12 @@ function handlePostRequest(req, res) {
         const parsedData = JSON.parse(body);
         const username = parsedData.name;
         users[username] = username;
-        return res.end(`application/json 으로 데이터를 받음" body: ${body} json: ${parsedData}`);
+        // return res.end(`application/json 으로 데이터를 받음" body: ${body} json: ${parsedData}`);
+        res.end(JSON.stringify(users));
+      } else if (req.headers["content-type"] === "application/x-www-form-urlencoded") {
+        console.log(req.headers);
+        res.writeHead(200, { "Content-Type": "application/json; charset=utf-8" });
+        return res.end("form으로 데이터를 잘 받았음");
       } else {
         res.writeHead(404);
         return res.end("모르는 타입");
@@ -71,14 +104,23 @@ function handlePostRequest(req, res) {
   }
 }
 
-function handlePutRequest(req, res) {
-  if (req.method === "PUT") {
-  }
-  res.end("PUT 요청 응답 완료");
-}
+function handlePutRequest(req, res) {}
 
 function handleDeleteRequest(req, res) {
-  res.end("DELETE 요청 응답 완료");
+  if (req.url.startsWith("/user/")) {
+    const username = path.basename(req.url);
+    if (username && users[username]) {
+      delete users[username]; // 객체 안의 멤버 삭제
+      res.writeHead(200, { "Content-type": "text-plain; charset=utf-8" });
+      res.end(`${username} 삭제 성공`);
+    } else {
+      res.writeHead(404, { "Content-type": "text-plain; charset=utf-8" });
+      res.end(`${username} 사용자를 찾을 수 없습니다`);
+    }
+  } else {
+    res.writeHead(404);
+    res.end("Not Found");
+  }
 }
 
 server.listen(3000, () => {
--- 4.nodejs/7.was/3.crud/static/user.js
+++ 4.nodejs/7.was/3.crud/static/user.js
@@ -1,32 +1,107 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("나의 js 코드");
-
-  const userTable = document.getElementById("userTable");
   const form = document.getElementById("form");
+  const username = document.getElementById("username");
 
-  // form 버튼을 클릭해서 post
+  updateUsers();
+
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    // 폼 자체의 기본 기능 못하게 막기
-    fetch("/user", {
-      method: "POST",
-    }).then();
-  });
 
-  // 시작할때 자동 로딩
+    const name = username.value;
+    if (!name) {
+      alert("이름 입력하세요");
+      return;
+    }
+
+    registerUser(name);
+  });
+});
+
+function registerUser(name) {
   fetch("/user", {
-    method: "GET",
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify({ name }), // name=name 의 값
   })
     .then((response) => {
       if (response.ok) {
-        console.log("성공");
-        return response.json();
+        // alert("등록 성공");
+        username.value = "";
+        updateUsers();
       } else {
-        console.log("에러");
+        alert("등록 실패");
       }
     })
-    // 위에서 리턴한 response.json 의 결과를 받아서 처리하는 곳
-    .then((data) => {
-      console.log(data);
+    .catch((error) => {
+      alert(`등록 중에 오류 발생 ${error.message}`);
     });
-});
+}
+
+function updateUsers() {
+  fetch("/user")
+    .then((response) => {
+      const result = response.json();
+      // console.log(result);
+      return result;
+    })
+    .then((users) => {
+      // 결과 출력할 곳?
+      console.log(users);
+      const userTable = document.getElementById("userTable");
+      userTable.innerText = "";
+
+      if (Object.keys(users).length === 0) {
+        // 사용자가 없는 경우
+        const row = document.createElement("div");
+        row.textContent = "등록된 사용자가 없습니다";
+        userTable.appendChild(row);
+      } else {
+        // 사용자가 한명이라도 있는 경우
+        for (const key in users) {
+          const row = document.createElement("div");
+          row.innerText = `ID: ${key}, Name: ${users[key]}`;
+          userTable.appendChild(row);
+
+          // 수정 버튼 추가
+          const modifyButton = document.createElement("button");
+          modifyButton.textContent = "수정";
+          row.appendChild(modifyButton);
+
+          // 삭제 버튼 추가
+          const deleteButton = document.createElement("button");
+          deleteButton.textContent = "삭제";
+          deleteButton.addEventListener("click", () => deleteUser(key));
+          row.appendChild(deleteButton);
+
+          // 위에 모든 내용(즉 한줄을 테이블에 삽입)
+          userTable.appendChild(row);
+        }
+      }
+    })
+    .catch((error) => {
+      console.error("사용자를 불러오기 실패", error.message);
+      alert("사용자 로딩 오류");
+    });
+}
+
+function deleteUser(userId) {
+  fetch(`/user/${userId}`, {
+    method: "DELETE",
+  })
+    .then((response) => {
+      if (response.ok) {
+        // alert("삭제 성공");
+        updateUsers();
+      } else {
+        alert("삭제 실패");
+      }
+    })
+    .catch((error) => {
+      console.error("삭제 중 오류??", error.message);
+      alert("삭제 중 오류가 발생");
+    });
+}
+
+function modifyUser(userId) {
+  const name = prompt(username);
+}
