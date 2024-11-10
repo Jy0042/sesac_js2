bbd27e5cb1000de563e3e7e2de86613eed30805d
jy0042 <sjy6630@gmail.com>
2024-11-10 21:16:26 +0900

    11월 09일 express 수업

--- 4.nodejs/12.session/3.login/app.js
+++ 4.nodejs/12.session/3.login/app.js
@@ -1,25 +1,42 @@
 const express = require("express");
 const session = require("express-session");
 const path = require("path");
-const { send } = require("process");
 
 const app = express();
 const port = 3000;
+const sessionTime = 10000;
 
 // 미들웨어
 app.use("/public", express.static("public"));
 
 // 요청 바디에 json이 있으면 req.body에 담기
-// app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
 app.use(
   session({
     secret: "my-secret-key",
     resave: false,
     saveUninitialized: true,
+    cookie: {
+      maxAge: sessionTime,
+    },
   })
 );
+
+// app.use((req, res, next) => {
+//   if (req.session.user) {
+//     req.session.touch();
+//     req.session.cookie.maxAge = sessionTime; // 세션 만료 시간 재설정
+
+//     const remainingTime = req.session.cookie.expires - Date.now();
+//     console.log("세션 남은 시간 (밀리초):", remainingTime);
+//   }
+//   next();
+// });
+
+// static 폴더 정의
+app.use("/static", express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "login.html"));
@@ -30,18 +47,26 @@ const users = [
     id: 1,
     username: "user1",
     password: "pw123",
+    hobby: "sleeping",
   },
   {
     id: 2,
     username: "user2",
     password: "pw123",
+    hobby: "studying",
+  },
+  {
+    id: 3,
+    username: "user3",
+    password: "pw123",
+    hobby: "walking",
   },
 ];
 
 app.post("/login", (req, res) => {
   // 로그인 코드 개발
   const { username, password } = req.body; // 미들웨어로 파서 추가
-  console.log("req.body:", req.body);
+  console.log(`사용자로 부터 받아온 username ${username} , password ${password}`);
 
   const user = users.find((userAccount) => {
     return userAccount.username === username && userAccount.password === password;
@@ -49,6 +74,8 @@ app.post("/login", (req, res) => {
 
   // 사용자가 입력한 id/pw 를 위에 users 자료 구조에서 검색
   if (user) {
+    req.session.user = user;
+
     console.log(`username: ${username}, password: ${password}`);
     return res.json({ message: "로그인 성공" });
   } else {
@@ -57,11 +84,11 @@ app.post("/login", (req, res) => {
 });
 
 app.get("/profile", (req, res) => {
-  const user = req.session.users;
+  const user = req.session.user;
   console.log("/profile:user", user);
 
   if (user) {
-    res.json({ username: user.username, message: "프로필 정보" });
+    res.json({ username: user.username, hobby: user.hobby, message: "프로필 정보" });
   } else {
     res.status(401).json({ message: "인증되지 않은 사용자" });
   }
@@ -69,7 +96,26 @@ app.get("/profile", (req, res) => {
 
 // 로그아웃은 어떻게?
 app.get("/logout", (req, res) => {
-  // 세션에서 사용자 정보를 삭제. 어떻게? => 찾아본다
+  const user = req.session.user;
+
+  if (user && user.username) {
+    req.session.destroy();
+    res.json({ message: "로그아웃 하였습니다" });
+  } else {
+    res.json({ message: "로그인 상태가 아닙니다" });
+  }
+});
+
+app.get("/check-login", (req, res) => {
+  const user = req.session.user;
+
+  console.log("/check-login", user);
+
+  if (user) {
+    res.json({ username: user.username });
+  } else {
+    res.status(401).send();
+  }
 });
 
 app.listen(port, () => {
---
+++ 4.nodejs/12.session/3.login/public/login_241110.js
@@ -0,0 +1,106 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("loginButton").addEventListener("click", login);
+  document.getElementById("logoutButton").addEventListener("click", logout);
+
+  checkLoginStatus();
+});
+
+function checkLoginStatus() {
+  fetch("/check-login")
+    .then((response) => {
+      if (response.ok) {
+        // showProfile(username);
+        return response.json();
+      } else {
+        console.log("로그인 상태가 아님");
+        throw new Error("로그인 안 된 사용자");
+      }
+    })
+    .then((data) => {
+      if (data && data.username) {
+        showProfile(data.username);
+      }
+    })
+    .catch((error) => {
+      console.log("로그인 상태가 아닙니다");
+    });
+}
+
+// function checkLoginStatus() {
+//   try {
+//     const response = fetch("/check-login");
+
+//     if (response.ok) {
+//       const data = response.json();
+//       console.log("checkLoginStatus:", data);
+//       if (data && data.username) {
+//         showProfile(data.username);
+//       }
+//       // showProfile(username);
+//       return data;
+//     } else {
+//       console.log("로그인 상태가 아님");
+//       throw new Error("로그인 안 된 사용자");
+//     }
+//   } catch (error) {
+//     console.error("로그인 상태가 아닙니다");
+//   }
+// }
+
+// 위에 코드 async/await 으로 변경
+async function checkLoginStatusAsyncAwait() {
+  try {
+    const response = fetch("/check-login");
+    const data = await response.json();
+
+    if (data && data.username) {
+      console.log(data.username);
+      showProfile(data.username);
+    } else {
+      showLoginForm();
+    }
+  } catch {
+    console.log("로그인 상태가 아닙니다");
+    showLoginForm();
+  }
+}
+
+function logout() {
+  fetch("/logout").then((response) => {
+    if (response.ok) {
+      showLoginForm();
+    }
+  });
+}
+
+function login(e) {
+  e.preventDefault();
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  console.log("로그인 버튼 클릭");
+
+  fetch("/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password }),
+  }).then((response) => {
+    if (response.ok) {
+      console.log("로그인 성공");
+      // window.location.href = "/profile";
+      showProfile(username);
+    } else {
+      console.log("로그인 실패");
+    }
+  });
+}
+
+function showProfile(username) {
+  document.getElementById("loginFormContainer").style.display = "none";
+  document.getElementById("profile").style.display = "block";
+  document.getElementById("usernameSpan").innerText = username;
+}
+function showLoginForm() {
+  document.getElementById("loginFormContainer").style.display = "block";
+  document.getElementById("profile").style.display = "none";
+}
